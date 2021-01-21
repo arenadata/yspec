65c4a7e1410f470cf48613fe08d7a8a40ee70f51
Konstantin Voschanov <vka@arenadata.io>
2021-01-21 15:19:12 +0300

    ADCM-1550 improve error processing

--- yspec/checker.py
+++ yspec/checker.py
@@ -16,12 +16,22 @@
 # under the License.
 
 
+import ruyaml
+
+
 class FormatError(Exception):
-    def __init__(self, path, message, data=None, caused_by=None):
-        self.path = fp(path)
+    def __init__(self, path, message, data=None, caused_by=None, rule=None, parent=None):
+        self.path = path
         self.message = message
         self.data = data
+        self.rule = rule
         self.errors = caused_by
+        self.parent = parent
+        self.line = None
+        if isinstance(data, ruyaml.comments.CommentedBase):
+            self.line = data.lc.line
+        elif parent:
+            self.line = parent.lc.line
         super().__init__(message)
 
     def __str__(self):
@@ -36,52 +46,74 @@ class SchemaError(Exception):
     pass
 
 
+class DataError(Exception):
+    pass
+
+
 def fp(path):
     return '/'.join(path)
 
 
-def check_type(data, data_type, path):
+def check_type(data, data_type, path, rule=None, parent=None):
     if not isinstance(data, data_type):
-        raise FormatError(path, 'Object should be a {}'.format(str(data_type)), data)
+        last = path[-1]
+        raise FormatError(
+            path,
+            '{} "{}" should be a {}'.format(last[0], last[1], str(data_type)),
+            data,
+            rule=rule,
+            parent=parent
+        )
 
 
-def match_list(data, rules, rule, path):
-    check_type(data, list, path)
+def match_list(data, rules, rule, path, parent=None):
+    check_type(data, list, path, rule, parent=parent)
     for i, v in enumerate(data):
-        process_rule(v, rules, rule['item'], path + [str(i)])
+        process_rule(
+            v, rules, rule['item'], path=path + [('Value of list index', i)], parent=parent
+        )
     return True
 
 
-def match_dict(data, rules, rule, path):
-    check_type(data, dict, path)
+def match_dict(data, rules, rule, path, parent=None):
+    check_type(data, dict, path, rule)
     if 'required_items' in rule:
         for i in rule['required_items']:
             if i not in data:
-                raise FormatError(path, f"There is no required item {i} in dict", data)
-    for k, v in data.items():
+                raise FormatError(path, f'There is no required key "{i}" in map', data, rule=rule)
+    for k in data:
+        new_path = path + [('Value of map key', k)]
         if 'items' in rule and k in rule['items']:
-            process_rule(data[k], rules, rule['items'][k], path + [k])
+            process_rule(data[k], rules, rule['items'][k], new_path, parent=data)
         elif 'default_item' in rule:
-            process_rule(data[k], rules, rule['default_item'], path + [k])
+            process_rule(data[k], rules, rule['default_item'], new_path, parent=data)
         else:
-            raise FormatError(path, f"No item rule for {k}", data)
+            raise FormatError(path, f'Map key "{k}" is not allowed here', data, rule=rule)
 
 
-def match_dict_key_selection(data, rules, rule, path):
-    check_type(data, dict, path)
+def match_dict_key_selection(data, rules, rule, path, parent=None):
+    check_type(data, dict, path, rule, parent=parent)
     key = rule['selector']
     if key not in data:
-        raise FormatError(path, f"There is no type in that dict", data)
+        raise FormatError(
+            path, f'There is no key "{key}" in that map.', data, rule=rule, parent=parent
+        )
     value = data[key]
     if value in rule['variants']:
-        process_rule(data, rules, rule['variants'][value], path)
+        process_rule(data, rules, rule['variants'][value], path, parent=parent)
     elif 'default_variant' in rule:
-        process_rule(data, rules, rule['default_variant'], path)
+        process_rule(data, rules, rule['default_variant'], path, parent=parent)
     else:
-        raise FormatError(path, f"There is no '{key}: {value}' allowed here.", data)
+        raise FormatError(
+            path,
+            f'Value "{value}" is not allowed for map key "{key}".',
+            data,
+            rule=rule,
+            parent=parent
+        )
 
 
-def match_one_of(data, rules, rule, path):
+def match_one_of(data, rules, rule, path, parent=None):
     errors = []
     for obj in rule['variants']:
         try:
@@ -93,8 +125,8 @@ def match_one_of(data, rules, rule, path):
 
 
 def match_simple_type(obj_type):
-    def match(data, rules, rule, path):
-        check_type(data, obj_type, path)
+    def match(data, rules, rule, path, parent=None):
+        check_type(data, obj_type, path, rule, parent=parent)
     return match
 
 
@@ -112,7 +144,7 @@ MATCH = {
 
 def check_rule(rules):
     if not isinstance(rules, dict):
-        return (False, 'YSpec should be a dict')
+        return (False, 'YSpec should be a map')
     if 'root' not in rules:
         return (False, 'YSpec should has "root" key')
     if 'match' not in rules['root']:
@@ -120,16 +152,25 @@ def check_rule(rules):
     return (True, '')
 
 
-def process_rule(data, rules, name, path=None):
+def process_rule(data, rules, name, path=None, parent=None):
     if path is None:
-        path = ['.']
+        path = []
     if name not in rules:
         raise SchemaError(f"There is no rule {name} in schema.")
     rule = rules[name]
     if 'match' not in rule:
-        raise SchemaError(f"There is no mandatory match attr in rule in schema.")
+        raise SchemaError(f"There is no mandatory match attr in rule {rule} in schema.")
     match = rule['match']
     if match not in MATCH:
         raise SchemaError(f"Unknown match {match} from schema. Donno how to handle that.")
 
-    MATCH[match](data, rules, rule, path)
+    # print(f'process_rule: {MATCH[match]}.__name__ "{name}" path: {path}, data: {data}')
+    MATCH[match](data, rules, rule, path=path, parent=parent)
+
+
+def check(data, rules):
+    if not isinstance(data, ruyaml.comments.CommentedBase):
+        raise DataError("You should use ruyaml.round_trip_load() to parse date yaml")
+    if not isinstance(rules, ruyaml.comments.CommentedBase):
+        raise SchemaError("You should use ruyaml.round_trip_load() to parse schema yaml")
+    process_rule(data, rules, 'root')
