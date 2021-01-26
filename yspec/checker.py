4b10833df8d787c00b87053bf1046e32d232bc58
Konstantin Voschanov <vka@arenadata.io>
2021-01-26 18:14:50 +0300

    clear code

--- yspec/checker.py
+++ yspec/checker.py
@@ -20,7 +20,7 @@ import ruyaml
 
 
 class FormatError(Exception):
-    def __init__(self, path, message, data=None, caused_by=None, rule=None, parent=None):
+    def __init__(self, path, message, data=None, rule=None, parent=None, caused_by=None):
         self.path = path
         self.message = message
         self.data = data
@@ -34,46 +34,36 @@ class FormatError(Exception):
             self.line = parent.lc.line
         super().__init__(message)
 
-    def __str__(self):
-        message = f"at {self.path}: {self.message}"
-        if self.errors is not None:
-            for e in self.errors:
-                message = message + "\n" + str(e)
-        return message
-
 
 class SchemaError(Exception):
-    def __init__(self, message, rule=None):
-        self.rule = rule
-        self.line = None
-        if isinstance(rule, ruyaml.comments.CommentedBase):
-            self.line = rule.lc.line
-        super().__init__(message)
-
+    pass
 
 class DataError(Exception):
     pass
 
 
-def fp(path):
-    return '/'.join(path)
-
-
 def check_type(data, data_type, path, rule=None, parent=None):
     if not isinstance(data, data_type):
+        msg = 'Object should be a {}'.format(str(data_type))
         if path:
             last = path[-1]
             msg = '{} "{}" should be a {}'.format(last[0], last[1], str(data_type))
-        else:
-            msg = 'Object should be a {}'.format(str(data_type))
-        raise FormatError(path, msg, data, rule=rule, parent=parent)
+        raise FormatError(path, msg, data, rule, parent)
+
+
+def check_match_type(match, data, data_type, path, rule, parent=None):
+    if not isinstance(data, data_type):
+        msg = f'Input data for {match}, rule "{rule}" should be {str(data_type)}"'
+        raise FormatError(path, msg, data, rule, parent)
 
 
 def match_none(data, rules, rule, path, parent=None):
     if data is not None:
-        last = path[-1]
-        msg = '{} "{}" should be a None'.format(last[0], last[1])
-        raise FormatError(path, msg, data, rule=rule, parent=parent)
+        msg = 'Object should be empty'
+        if path:
+            last = path[-1]
+            msg = '{} "{}" should be empty'.format(last[0], last[1])
+        raise FormatError(path, msg, data, rule, parent)
 
 
 def match_any(data, rules, rule, path, parent=None):
@@ -81,63 +71,65 @@ def match_any(data, rules, rule, path, parent=None):
 
 
 def match_list(data, rules, rule, path, parent=None):
-    check_type(data, list, path, rule, parent=parent)
+    check_match_type('match_list', data, list, path, rule, parent)
     for i, v in enumerate(data):
-        process_rule(
-            v, rules, rule['item'], path=path + [('Value of list index', i)], parent=parent
-        )
+        process_rule(v, rules, rules[rule]['item'], path + [('Value of list index', i)], parent)
     return True
 
 
 def match_dict(data, rules, rule, path, parent=None):
-    check_type(data, dict, path, rule=rule, parent=parent)
-    if 'required_items' in rule:
-        for i in rule['required_items']:
+    check_match_type('match_dict', data, dict, path, rule, parent)
+    if 'required_items' in rules[rule]:
+        for i in rules[rule]['required_items']:
             if i not in data:
-                raise FormatError(path, f'There is no required key "{i}" in map', data, rule=rule)
+                raise FormatError(path, f'There is no required key "{i}" in map', data, rule)
     for k in data:
         new_path = path + [('Value of map key', k)]
-        if 'items' in rule and k in rule['items']:
-            process_rule(data[k], rules, rule['items'][k], new_path, parent=data)
-        elif 'default_item' in rule:
-            process_rule(data[k], rules, rule['default_item'], new_path, parent=data)
+        if 'items' in rules[rule] and k in rules[rule]['items']:
+            process_rule(data[k], rules, rules[rule]['items'][k], new_path, data)
+        elif 'default_item' in rules[rule]:
+            process_rule(data[k], rules, rules[rule]['default_item'], new_path, data)
         else:
-            raise FormatError(path, f'Map key "{k}" is not allowed here', data, rule=rule)
+            msg = f'Map key "{k}" is not allowed here (rule "{rule}")'
+            raise FormatError(path, msg, data, rule)
 
 
 def match_dict_key_selection(data, rules, rule, path, parent=None):
-    check_type(data, dict, path, rule, parent=parent)
-    key = rule['selector']
+    check_match_type('dict_key_selection', data, dict, path, rule, parent)
+    key = rules[rule]['selector']
     if key not in data:
-        raise FormatError(
-            path, f'There is no key "{key}" in that map.', data, rule=rule, parent=parent
-        )
+        msg = f'There is no key "{key}" in that map.'
+        raise FormatError(path, msg, data, rule, parent)
     value = data[key]
-    if value in rule['variants']:
-        process_rule(data, rules, rule['variants'][value], path, parent=parent)
+    if value in rules[rule]['variants']:
+        process_rule(data, rules, rules[rule]['variants'][value], path, parent)
     elif 'default_variant' in rule:
-        process_rule(data, rules, rule['default_variant'], path, parent=parent)
+        process_rule(data, rules, rules[rule]['default_variant'], path, parent)
     else:
         msg = f'Value "{value}" is not allowed for map key "{key}".'
-        raise FormatError(path, msg, data, rule=rule, parent=parent)
+        raise FormatError(path, msg, data, rule, parent)
 
 
 def match_one_of(data, rules, rule, path, parent=None):
     errors = []
-    for obj in rule['variants']:
+    sub_errors = []
+    for obj in rules[rule]['variants']:
         try:
-            process_rule(data, rules, obj, path)
+            process_rule(data, rules, obj, path, parent)
         except FormatError as e:
+            if e.errors:
+                sub_errors += e.errors
             errors.append(e)
-    if len(errors) == len(rule['variants']):
-        raise errors[-1]
-        #raise FormatError(path, "None of the variants match", data, errors, rule)
+    if len(errors) == len(rules[rule]['variants']):
+        errors += sub_errors
+        msg = f'None of the variants for rule "{rule}" match'
+        raise FormatError(path, msg, data, rule, parent, caused_by=errors)
 
 
 def match_set(data, rules, rule, path, parent=None):
-    if data not in rule['variants']:
-        msg = f'Value "{data}" not in set {rule["variants"]}'
-        raise FormatError(path, msg, data, rule=rule, parent=parent)
+    if data not in rules[rule]['variants']:
+        msg = f'Value "{data}" not in set {rules[rule]["variants"]}'
+        raise FormatError(path, msg, data, rule, parent=parent)
 
 
 def match_simple_type(obj_type):
@@ -183,13 +175,13 @@ def process_rule(data, rules, name, path=None, parent=None):
     if match not in MATCH:
         raise SchemaError(f"Unknown match {match} from schema. Donno how to handle that.")
 
-    #print(f'process_rule: {MATCH[match].__name__} "{name}" path: {path}, data: {data}')
-    MATCH[match](data, rules, rule, path=path, parent=parent)
+    #print(f'process_rule: {MATCH[match].__name__} "{name}" data: {data}')
+    MATCH[match](data, rules, name, path=path, parent=parent)
 
 
 def check(data, rules):
     if not isinstance(data, ruyaml.comments.CommentedBase):
-        raise DataError("You should use ruyaml.round_trip_load() to parse date yaml")
+        raise DataError("You should use ruyaml.round_trip_load() to parse data yaml")
     if not isinstance(rules, ruyaml.comments.CommentedBase):
         raise SchemaError("You should use ruyaml.round_trip_load() to parse schema yaml")
     process_rule(data, rules, 'root')
